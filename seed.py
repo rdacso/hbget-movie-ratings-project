78e04bd7305eb6f2e1c4d095f15fb96c7203b032
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-24 18:02:18 -0700

    started creating databases

--- seed.py
+++ seed.py
@@ -1,12 +1,14 @@
 """Utility file to seed ratings database from MovieLens data in seed_data/"""
 
 from sqlalchemy import func
-from model import User
+from model import User, Rating, Movie
 # from model import Rating
 # from model import Movie
 
 from model import connect_to_db, db
 from server import app
+from datetime import datetime
+
 
 
 def load_users():
@@ -37,6 +39,28 @@ def load_users():
 def load_movies():
     """Load movies from u.item into database."""
 
+    print "Movies"
+
+    Movie.query.delete()
+    for row in open("seed_data/u.item"):
+        row = row.rstrip()
+        movie_id, title, released_at, _EMPTY, imdb_url = row.split("|")[:5]
+        # released_at = datetime.strptime(released_at, "%d-%b-%Y")
+        print released_at
+        print title
+
+        
+    print type(released_at)
+
+        # movie =  Movie(movie_id=movie_id,
+        #             title=title,
+        #             released_at=released_at,
+        #             imdb_url=imdb_url)
+
+    #     db.session.add(user)
+
+    # # Once we're done, we should commit our work
+    # db.session.commit()
 
 def load_ratings():
     """Load ratings from u.data into database."""
